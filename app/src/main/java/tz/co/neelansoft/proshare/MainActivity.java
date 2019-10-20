ebfe52bbf2e19c247889954274184797c71b522c
Landry Kapela <landrykapela@gmail.com>
2019-10-20 11:28:52 +0300

    Add Loan application form

--- app/src/main/java/tz/co/neelansoft/proshare/MainActivity.java
+++ app/src/main/java/tz/co/neelansoft/proshare/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 	boolean firstTime = true;
+	ImageView ivSettings;
+	ImageView ivSignout;
+	ImageView ivRequest;
+	ImageView ivCheck;
+	ImageView ivRepay;
 	
 	@Override
 	protected void onStart(){
@@ -24,5 +30,11 @@ public class MainActivity extends Activity {
 		if(getIntent().hasExtra("first_run")){
 			firstTime = getIntent().getBooleanExtra("first_run",false);
 		}
+
+		ivCheck = findViewById(R.id.ivStatus);
+		ivRepay = findViewById(R.id.ivPay);
+		ivRequest = findViewById(R.id.ivRequest);
+		ivSettings = findViewById(R.id.ivSettings);
+		ivSignout = findViewById(R.id.ivExit);
 	}
 }
